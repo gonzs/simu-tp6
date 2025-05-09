5647a19e618acef9c4b5d1c7304fd01dda91373c
gonzs <gonzalosisnero@gmail.com>
2025-05-09 05:41:21 +0000

    more fix lint

--- src/utils/reserva.py
+++ src/utils/reserva.py
@@ -1,11 +1,11 @@
-   def asignar_reserva(self, habitacion, fecha_fin):
-        """Asigna una reserva a una habitación y calcula el tiempo ocioso si estaba vacía."""
-        if habitacion["comprometida_hasta"] < self.tiempo_actual:
-            # Calcular tiempo ocioso
-            tiempo_ocioso = self.tiempo_actual - habitacion["comprometida_hasta"]
-            habitacion.setdefault("tiempo_ocioso", 0)
-            habitacion["tiempo_ocioso"] += tiempo_ocioso
+def asignar_reserva(self, habitacion, fecha_fin):
+    """Asigna una reserva a una habitación y calcula el tiempo ocioso si estaba vacía."""
+    if habitacion["comprometida_hasta"] < self.tiempo_actual:
+        # Calcular tiempo ocioso
+        tiempo_ocioso = self.tiempo_actual - habitacion["comprometida_hasta"]
+        habitacion.setdefault("tiempo_ocioso", 0)
+        habitacion["tiempo_ocioso"] += tiempo_ocioso
 
-        # Asignar la nueva reserva
-        habitacion["comprometida_hasta"] = fecha_fin
-        self.total_huespedes += 1
+    # Asignar la nueva reserva
+    habitacion["comprometida_hasta"] = fecha_fin
+    self.total_huespedes += 1
