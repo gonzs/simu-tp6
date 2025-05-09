ccb552627615ae7f91a4f6a65a753244b172f1bf
gonzs <gonzalosisnero@gmail.com>
2025-05-09 06:06:25 +0000

    fix export

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -4,4 +4,8 @@ from .graficos import *
 from .reserva import *
 
 class HotelUtils:
-    pass
+    encontrar_habitacion_disponible = staticmethod(encontrar_habitacion_disponible)
+    encontrar_cunas_disponibles = staticmethod(encontrar_cunas_disponibles)
+    encontrar_camas_disponibles = staticmethod(encontrar_camas_disponibles)
+    asignar_reserva = staticmethod(asignar_reserva)
+    imprimir_resultados = staticmethod(imprimir_resultados)
