2c9cd0b8cdbdcad1fb4c99e400cd06bc005cca97
gonzs <gonzalosisnero@gmail.com>
2025-05-09 02:10:12 -0300

    add events

---
+++ src/eventos/llegada_huesped.py
@@ -0,0 +1,62 @@
+def llegada_huesped(self):
+        """Tratar un evento de llegada de huésped"""
+        # Generar parámetros del huésped
+        duracion_estadia = self.generar_duracion_estadia()
+        tipo_habitacion = self.generar_tipo_habitacion()
+        num_ninios = self.generar_num_ninios()
+        num_bebes = self.generar_num_bebes()
+
+        self.total_arribos += 1
+
+        # Calcular tiempo de finalización de la estadía
+        fecha_fin = self.tiempo_actual + duracion_estadia
+        
+        # Intentar encontrar una habitación disponible
+        id_habitacion = self.encontrar_habitacion_disponible(tipo_habitacion, self.tiempo_actual)
+
+        if id_habitacion is not None:
+            # Chequear si las cunas necesarias están disponibles
+            cunas_necesarias = min(num_bebes, 2)
+            id_cunas = None
+            if cunas_necesarias > 0:
+                id_cunas = self.encontrar_cunas_disponibles(cunas_necesarias, self.tiempo_actual)
+
+            # Chequear si las camas necesarias están disponibles
+            camas_necesarias = min(num_ninios, 2) 
+            id_camas = None
+            if camas_necesarias > 0:
+                id_camas = self.encontrar_camas_disponibles(camas_necesarias, self.tiempo_actual)
+
+            # Allocate room
+            if tipo_habitacion == "simple":
+                self.asignar_reserva(self.habitaciones_simples[id_habitacion], fecha_fin)
+            elif tipo_habitacion == "double":
+                self.asignar_reserva(self.habitaciones_dobles[id_habitacion], fecha_fin)
+            elif tipo_habitacion == "suite":
+                self.asignar_reserva(self.habitaciones_suites[id_habitacion], fecha_fin)
+            
+            # Si todos los recursos están disponibles
+            # (camas y cunas) se asignan a la habitación
+            # Si no hay cunas o camas necesarias, se asignan
+            # las que están disponibles
+            if (cunas_necesarias == 0 or id_cunas) and (camas_necesarias == 0 or id_camas):
+                # Asignar cunas si es necesario
+                if id_cunas:
+                    for id_cuna in id_cunas:
+                        self.cunas[id_cuna]["comprometida_hasta"] = fecha_fin
+
+                # Asignar camas si es necesario
+                if id_camas:
+                    for id_cama in id_camas:
+                        self.camas_simples[id_cama]["comprometida_hasta"] = fecha_fin
+
+                return True
+            else:
+                # Si no estan disponibles todos los recursos adicionales
+                # se aplica una bonificación
+                self.bonificaciones += 1
+                return False
+        else:
+            # Si no hay habitación disponible, se rechaza la reserva
+            self.reservas_rechazadas += 1
+            return False
