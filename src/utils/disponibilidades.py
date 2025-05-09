bde08ce6e93ed339b0000503a7b1d9bd4530d9fa
gonzs <gonzalosisnero@gmail.com>
2025-05-09 05:37:18 +0000

    fix lint

--- src/utils/disponibilidades.py
+++ src/utils/disponibilidades.py
@@ -19,32 +19,32 @@ def encontrar_habitacion_disponible(self, tipo_habitacion, fecha_inicio):
         # Devolver la primera habitación disponible si existe
         return habitaciones_disponibles[0][0] if habitaciones_disponibles else None
 
-    def encontrar_cunas_disponibles(self, numero_solicitado, fecha_inicio):
-        """Encontrar las cunas disponibles para el período de tiempo dado."""
-        cunas_disponibles = []
-        for i, cuna in enumerate(self.cunas):
-            if cuna["comprometida_hasta"] <= fecha_inicio:
-                cunas_disponibles.append((i, cuna["comprometida_hasta"]))
+def encontrar_cunas_disponibles(self, numero_solicitado, fecha_inicio):
+    """Encontrar las cunas disponibles para el período de tiempo dado."""
+    cunas_disponibles = []
+    for i, cuna in enumerate(self.cunas):
+        if cuna["comprometida_hasta"] <= fecha_inicio:
+            cunas_disponibles.append((i, cuna["comprometida_hasta"]))
 
-        # Ordenar las cunas por la fecha en que se liberan (comprometida_hasta)
-        cunas_disponibles.sort(key=lambda x: x[1])
+    # Ordenar las cunas por la fecha en que se liberan (comprometida_hasta)
+    cunas_disponibles.sort(key=lambda x: x[1])
 
-        # Seleccionar las cunas necesarias
-        cunas_seleccionadas = [cuna[0] for cuna in cunas_disponibles[:numero_solicitado]]
+    # Seleccionar las cunas necesarias
+    cunas_seleccionadas = [cuna[0] for cuna in cunas_disponibles[:numero_solicitado]]
 
-        return cunas_seleccionadas if len(cunas_seleccionadas) == numero_solicitado else None
+    return cunas_seleccionadas if len(cunas_seleccionadas) == numero_solicitado else None
 
 def encontrar_camas_disponibles(self, numero_solicitado, fecha_inicio):
     """Encontrar camas disponibles para el período de tiempo dado"""
     camas_disponibles = []
-        for i, cama in enumerate(self.camas_simples):
-            if cama["comprometida_hasta"] <= fecha_inicio:
-                camas_disponibles.append((i, cama["comprometida_hasta"]))
+    for i, cama in enumerate(self.camas_simples):
+        if cama["comprometida_hasta"] <= fecha_inicio:
+            camas_disponibles.append((i, cama["comprometida_hasta"]))
 
-        # Ordenar las camas por la fecha en que se liberan (comprometida_hasta)
-        camas_disponibles.sort(key=lambda x: x[1])
-        
-        # Seleccionar las camas necesarias
-        camas_seleccionadas = [cama[0] for cama in camas_disponibles[:numero_solicitado]]
-        
-        return camas_seleccionadas if len(camas_seleccionadas) == numero_solicitado else None
+    # Ordenar las camas por la fecha en que se liberan (comprometida_hasta)
+    camas_disponibles.sort(key=lambda x: x[1])
+
+    # Seleccionar las camas necesarias
+    camas_seleccionadas = [cama[0] for cama in camas_disponibles[:numero_solicitado]]
+
+    return camas_seleccionadas if len(camas_seleccionadas) == numero_solicitado else None
