975906ed906f0f88f965d62aaec051c7b7a67528
gonzs <gonzalosisnero@gmail.com>
2025-05-10 21:09:35 +0000

    update fdp duracion

--- src/fdp/duracion_estadia.py
+++ src/fdp/duracion_estadia.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 import numpy as np
-from scipy.stats import gennorm
-import random
-import math
-from scipy.stats import norm
+from scipy.stats import dweibull
 
 def generar_duracion_estadia():
     """Generar duración de la estadía (en horas)"""
-    # PENDING: Suposición: Distribución normal con media de 3 días, desviación estándar de 1 día
-    R = random.random()  # R ~ U(0,1)
-    mu = math.log(507.69)
-    sigma = 0.04605
-    return math.exp(mu + sigma * norm.ppf(R)) - 435.97
+    # Distribución Weibull
+    # Parámetros de la distribución Weibull
+    c= 0.3510566987873381
+    loc= 2.9999999999999996
+    scale= 0.9263278639266157
+    R = np.random.rand()
+
+    # Generar IA con método de inversa (usando la ppf de scipy)
+    ia_inversa = dweibull.ppf(R, c=c, loc=loc, scale=scale).astype(int)
+
+    # Convertir de días a horas y asegurar entre 24 (1 día) y 192 (8 días)
+    duracion_en_horas = np.clip(ia_inversa, 1, 8) * 24
+    return duracion_en_horas
