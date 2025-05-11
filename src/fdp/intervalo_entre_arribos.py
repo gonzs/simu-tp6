2983ee26dba91af3b3120e7ed8e47e2b06be9316
gonzs <gonzalosisnero@gmail.com>
2025-05-11 00:26:24 +0000

    reverse fdp

--- src/fdp/intervalo_entre_arribos.py
+++ src/fdp/intervalo_entre_arribos.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 import numpy as np
 from scipy.stats import gamma
+import math
 
 def generar_intervalo_entre_arribos():
     """Generar intervalo entre llegadas de huéspedes (en horas)"""
-    # R = random.random()
-    # return -math.log(1 - R) / 0.499
+    R = np.random.random()
+    return -math.log(1 - R) / 0.499
 
-    # Distribución Gamma
-    # Parámetros de la distribución Gamma
-    a = 0.9493717645899579
-    loc = -1.1394098234608795e-26
-    scale = 0.09213445691039115
-    R = np.random.rand()
+    # # Distribución Gamma
+    # # Parámetros de la distribución Gamma
+    # a = 0.9493717645899579
+    # loc = -1.1394098234608795e-26
+    # scale = 0.09213445691039115
+    # R = np.random.rand()
 
-    # Generar con método de inversa (usando la ppf de scipy)
-    return gamma.ppf(R, a, loc=loc, scale=scale)
+    # # Generar con método de inversa (usando la ppf de scipy)
+    # return gamma.ppf(R, a, loc=loc, scale=scale)
    
